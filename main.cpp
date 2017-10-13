dbaa537759cb350eaed15356466d52875590bd45
MrFive <MrFive9999@qq.com>
2017-10-13 17:35:44 +0800

    repair an unknow comlior error (smileCry)

--- main.cpp
+++ main.cpp
@@ -40,9 +40,9 @@ void run_idle(int time)
     cout << time << ": Runway is idle." << endl;
 }
 int main() // Airport simulation program
-/* Pre: The user must supply the number of time intervals the simulation is to run, the expected number of planes arriving, the expected number of planes departing per time interval, and the maximum allowed size for runway queues.
-    Post: The program performs a random simulation of the airport, showing the status of the runway at each time interval, and prints out a summary of airport operation at the conclusion.
-    Uses: Classes Runway, Plane, Random and functions run_idle, initialize. */
+           /* Pre: The user must supply the number of time intervals the simulation is to run, the expected number of planes arriving, the expected number of planes departing per time interval, and the maximum allowed size for runway queues.
+           Post: The program performs a random simulation of the airport, showing the status of the runway at each time interval, and prints out a summary of airport operation at the conclusion.
+           Uses: Classes Runway, Plane, Random and functions run_idle, initialize. */
 {
     int end_time; // time to run simulation
     int queue_limit; // size of Runway queues
@@ -51,7 +51,7 @@ int main() // Airport simulation program
     initialize(end_time, queue_limit, arrival_rate, departure_rate);
     // (a) same input->same result every time, better for debugging
     Random variable;
-// (b) use different seed everytime the program runs
+    // (b) use different seed everytime the program runs
     // Random variable(false);
     Runway small_airport(queue_limit);
     for (int current_time = 0; current_time < end_time; current_time++) {
@@ -72,7 +72,7 @@ int main() // Airport simulation program
         }
         Plane moving_plane;
         switch (small_airport.activity(current_time, moving_plane)) {
-        // Let at most one Plane onto the Runway at current_time.
+            // Let at most one Plane onto the Runway at current_time.
             case land:
                 moving_plane.land(current_time);
                 break;
